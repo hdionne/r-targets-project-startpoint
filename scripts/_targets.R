6fbaea02bd16883ff872033fab12f8e6b29cc032
Henry Dionne <henrydionne@proton.me>
2026-09-03 14:31:31 -0400

    removed commits from a project with wrong remote.

--- scripts/_targets.R
+++ scripts/_targets.R
@@ -9,30 +9,14 @@ tar_config_set(
 )
 
 # List the libraries.
-libraries <- c(
-  'tidyverse',
-  'openxlsx2',
-  'broom'
-)
+libraries <- c()
 
 tar_option_set(
   packages = libraries,
-  seed = 968548
+  seed = # TODO
 )
 # Tell targets that there is a folder of scripts defining functions, ./scripts/functions/ and that these scripts should be run.
 tar_source('./scripts/functions/')
 
 # Place targets here
-tar_plan(
-  wb_file = './raw-data/TPIAT_Peds_Data.xlsx',
-  wb = wb_load(wb_file),
-  outcome_df_raw = load_outcome_df(wb),
-  outcome_a1c_df_raw = load_outcome_a1c_df(wb),
-  outcome_df = prep_outcome_df(outcome_df_raw, outcome_a1c_df_raw),
-  outcome_wilson_tests = calc_wilson_tests(outcome_df),
-  demo_df_raw = load_demographics(wb),
-  demo_df = prep_demographics(demo_df_raw),
-  demo_chisq_tests = calc_demo_chisq_tests(demo_df),
-  within_outcome_chisq_tests = calc_outcome_timepoint_chisq_tests(outcome_df),
-  between_outcome_chisq_tests = calc_outcome_age_chisq_tests(outcome_df)
-)
+tar_plan()
